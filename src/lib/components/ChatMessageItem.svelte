c21ee0a2002f373f6190346f53a4d42d92175067
Ian Sinnott <ian@iansinnott.com>
2023-04-11 06:58:17 +0900

    refactor

---
+++ src/lib/components/ChatMessageItem.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import type { ChatMessage } from "$lib/db";
+  import classNames from "classnames";
+  let _class: string = "";
+  export { _class as class };
+
+  export let item: ChatMessage;
+</script>
+
+<div class={classNames("", _class)}>
+  {item.content}
+</div>
