77f94d69c64874811da044965a2d6e600a39694d
Ian Sinnott <ian@iansinnott.com>
2023-04-14 04:46:19 +0800

    update release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,11 +3,19 @@
 main() {
   echo "Releasing new version..."
   echo "    PWD: $PWD"
+  echo
 
   local version=$(jq -r '.version' package.json)
   
   # Replace version in tauri.conf.json
-  sed -i '' -e "s/\"version\": \".*\"/\"version\": \"$version\"/g" tauri.conf.json
+  sed -i '' -e "s/\"version\": \".*\"/\"version\": \"$version\"/g" src-tauri/tauri.conf.json
+  
+  # amend last commit
+  git add src-tauri/tauri.conf.json
+  git commit --amend --no-edit
+
+  # upsert the tag. if running yarn version the tag will have been created already
+  git tag -a "v$version" -m "v$version"
 }
 
 main
