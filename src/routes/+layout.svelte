bd0bcc5d97a6c312ce1ba854b811752e76ba7b90
Ian Sinnott <ian@iansinnott.com>
2023-04-14 15:00:14 +0800

    improve loading styles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -59,7 +59,7 @@
     <slot />
     <SettingsModal />
   {:else}
-    <div class="flex items-center justify-center h-full">
+    <div class="flex items-center justify-center h-screen">
       <div class="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-zinc-700" />
     </div>
   {/if}
