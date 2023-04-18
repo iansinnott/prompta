866cca618b288a3ab7e26886d97b50af9dce7acb
Ian Sinnott <ian@iansinnott.com>
2023-04-19 01:24:01 +0800

    update db version

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,1 +1,6 @@
-export const DB_NAME = "chat_db-v4";
+/**
+ * This is the name of the db the app will connect to locally for now. Changing
+ * it effectively resets the db, without data loss since you can change it back.
+ * This is good for testing as if on a new system.
+ */
+export const DB_NAME = "chat_db-v8.1";
