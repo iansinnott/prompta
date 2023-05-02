d89e19c607e545cd7e12a5c371cb03b5119321b9
Ian Sinnott <ian@iansinnott.com>
2023-05-02 17:47:03 +0800

    style improvements

--- src/lib/components/ChatMessageItem.svelte
+++ src/lib/components/ChatMessageItem.svelte
@@ -42,7 +42,7 @@
         </button>
       </div>
     {:else}
-      <div class="w-5 h-5 flex items-center justify-center">
+      <div class="w-5 h-5 sm:w-6 sm:h-6 flex items-center justify-center">
         <code class="text-xl inline-block">
           {"#"}
         </code>
