4c56d848d67f061d0acafb727e7197f87c43dba3
Ian Sinnott <ian@iansinnott.com>
2023-05-15 00:13:08 +0800

    Wait longer in publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -5,11 +5,10 @@
 ## It is assumed this is run aver a verion tag. I.e. `npm run release && npm run gh:publish`
 ##
 
-set -e 
+set -e
 
 # if gh is not installed, fail
-if ! command -v gh &> /dev/null
-then
+if ! command -v gh &>/dev/null; then
     echo "gh could not be found"
     exit
 fi
@@ -18,9 +17,9 @@ fi
 npm run build && git push && git push --tags
 
 echo "Waiting for the release workflow to start..."
-sleep 5
+sleep 15
 
-latest_tag="$(git describe --tags $(git rev-list --tags --max-count=1))";
+latest_tag="$(git describe --tags $(git rev-list --tags --max-count=1))"
 
 # List command here is to get the run id so we don't have to use the interactive gh run watch.
 gh run watch "$(gh run list --limit 1 | awk '{print $7}')" && ./scripts/upload.sh && gh release edit --prerelease=false --latest $latest_tag
