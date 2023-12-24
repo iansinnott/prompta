8437023e8a940982090f91131cf70f2211f2bd7a
Ian Sinnott <3154865+iansinnott@users.noreply.github.com>
2023-12-25 00:50:06 +0800

    disable caching of openai instance, since the client options might change
    Could still cache it, but would need to cache by args. i.e. would need
    to memoize it not simply make it a singleton.

--- src/lib/llm/openai.ts
+++ src/lib/llm/openai.ts
@@ -1,18 +1,12 @@
 import { OpenAI, type ClientOptions } from "openai";
 
-let openai: OpenAI;
-
+// NOTE The openai client is not currently cached because the opts change at
+// runtime. namely the base url and api key.
 export const initOpenAi = (opts: ClientOptions) => {
-  if (openai) {
-    return openai;
-  }
-
-  openai = new OpenAI({
+  return new OpenAI({
     // The app is currently all browser-based, and the key is stored locally and
     // only sent to openai.
     dangerouslyAllowBrowser: true,
     ...opts,
   });
-
-  return openai;
 };
